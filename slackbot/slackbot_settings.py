37bdd0c9c5fd017fd917d931e8cf91e9b2744ecf
Masa <katsukatsu.masa.0522@gmail.com>
2021-03-12 19:33:47 +0900

    no change

---
+++ slackbot/slackbot_settings.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-1841717889987-1848302638516-u3PPBJ0mIO9lB85CJ0Ig4jfc"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "合言葉が違います"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
+
+# 投稿するslack上のチャンネル
+ERRORS_TO = 'スマートキー'
