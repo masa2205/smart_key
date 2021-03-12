0008b4e08d062c2c91d4e836c551d641578b7a2f
Masa <katsukatsu.masa.0522@gmail.com>
2021-03-12 19:42:12 +0900

    time module cut

--- slackbot/run.py
+++ slackbot/run.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 # coding: utf-8
 from slackbot.bot import Bot
-import time
 
 def main():
     bot = Bot()
     bot.run()
 
 if __name__ == "__main__":
-    time.sleep(3)
-    print('start slackbot')
     main()
